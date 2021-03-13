54d4e8ba7a72e115230c2cf5ceffe8cbe619a58a
aben20807 <aben20807@gmail.com>
2021-03-13 15:15:29 +0800

    Add all

---
+++ README.md
@@ -0,0 +1,22 @@
+# py-video-thumbnail
+
+Create thumbnail from a video (default 4x4).
+
+## Environment
+```bash
+$ virtualenv -p python3.6 venv
+$ source venv/bin/activate
+$ pip install opencv-python
+```
+
+## Usage
+
+```bash
+$ python pvt.py BigBuckBunny.mp4
+# Or
+$ python pvt.py *.mp4
+```
+
+## Result
+
+![](img/BigBuckBunny.jpg)
---
+++ pvt.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+helpDoc = """
+Create thumbnail from a video (default 4x4).
+usage:
+    python pvt.py [video path]
+require:
+    pip install opencv-python
+    Support Python3
+Author:
+    Huang Po-Hsuan (aben20807@gmail.com)
+GitHub:
+    https://github.com/aben20807/py-video-thumbnail
+"""
+print(helpDoc)
+
+import os
+import sys
+import cv2
+import time
+import numpy as np
+
+
+def concat_vh(list_2d):
+    return cv2.vconcat([cv2.hconcat(list_h) for list_h in list_2d])
+
+
+def video_to_frames(video_filename, num_of_frames):
+    """Extract frames from video"""
+    cap = cv2.VideoCapture(video_filename)
+
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT) * 0.94)
+    frames = []
+    if cap.isOpened() and total_frames > 0:
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        success, image = cap.read()
+        for i in range(1, num_of_frames + 1):
+            cap.set(cv2.CAP_PROP_POS_FRAMES, round(total_frames * i / num_of_frames))
+            success, image = cap.read()
+            if not success:
+                break
+            frames.append(image)
+    return frames
+
+
+def process(video_path, shape=(4, 4), img_format=".jpg"):
+    """Extract frames from the video and creates thumbnails for one of each"""
+    # Extract frames from video
+    frames = video_to_frames(video_path, shape[0] * shape[1])
+    thumbnail_shape = [shape, frames[0].shape]
+    thumbnail_shape = [i for sub in thumbnail_shape for i in sub]
+    frames = np.reshape(frames, thumbnail_shape)
+
+    # Generate and save combined thumbnail
+    thumbnail = concat_vh(frames)
+    thumbnail_size = (frames[0][0].shape[1], frames[0][0].shape[0])
+    thumbnail = cv2.resize(thumbnail, thumbnail_size, interpolation=cv2.INTER_AREA)
+    cv2.imwrite(os.path.splitext(video_path)[0] + img_format, thumbnail)
+
+
+if __name__ == "__main__":
+    for v in sys.argv[1:]:
+        process(v)
